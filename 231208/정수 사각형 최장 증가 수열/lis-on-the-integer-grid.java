bf205f03baed7a637933e9cce7fd58debd7e9fb7
nnindd <62933478+nnindd@users.noreply.github.com>
2023-12-08 11:16:14 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 877ms, Memory: 45MB, Status: Passed - CodeTree

--- 231208/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 231208/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -2,65 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, map[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokens;
 
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
+        int n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        ArrayList<int[]> list = new ArrayList<>();
+
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i], 1);
+        }
 
         for(int i=0; i<n; i++){
             tokens = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++){
                 map[i][j] = Integer.parseInt(tokens.nextToken());
+                list.add(new int[]{i, j, map[i][j]});
             }
         }
 
-        int answer = 0;
+        //작은 칸부터 4방 탐색해서 최대로 갈 수 있는 길이 선택
+        Collections.sort(list, (a, b) -> a[2] - b[2]);
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                //모든 시작점에 대해서 탐색
-                answer = Math.max(answer, bfs(i, j));
-            }
-        }
+        int[][] dir = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
-        System.out.println(answer);
-
-    }
-
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    private static int bfs(int x, int y){
         int max = 0;
-
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{x, y, 1});
-
-        boolean[][] v = new boolean[n][n];
-        v[x][y] = true;
-
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-
-            max = Math.max(max, now[2]);
+        for(int i=0; i<list.size(); i++){
+            int[] now = list.get(i);
 
             for(int d=0; d<4; d++){
                 int nx = now[0] + dir[d][0];
                 int ny = now[1] + dir[d][1];
 
-                if(!isRange(nx, ny) || v[nx][ny] || map[nx][ny] <= map[now[0]][now[1]]) continue;
+                if(!isRange(nx, ny, n) || map[nx][ny] <= map[now[0]][now[1]]) continue;
 
-                v[nx][ny] = true;
-                q.offer(new int[]{nx, ny, now[2] + 1});
+                dp[nx][ny] = Math.max(dp[nx][ny], dp[now[0]][now[1]] + 1);
+
+                max = Math.max(max, dp[nx][ny]);
             }
         }
 
-        return max;
-    }
+        System.out.println(max);
 
-    private static boolean isRange(int x, int y){
+    }
+    private static boolean isRange(int x, int y, int n){
         if(x<0||y<0||x>=n||y>=n) return false;
         return true;
     }
